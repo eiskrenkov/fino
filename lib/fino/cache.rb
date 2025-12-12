390b2f40bb81b13da60b76114d6f8e42c7f91362
Egor Iskrenkov <egor@iskrenkov.me>
2025-12-12 16:13:27 +0100

    Fix memory cache interface

--- lib/fino/cache.rb
+++ lib/fino/cache.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 
 module Fino::Cache
-  def fetch(key, &)
+  def exist?(key)
+    raise NotImplementedError
+  end
+
+  def read(key)
     raise NotImplementedError
   end
 
   def write(key, value)
+    raise NotImplementedError
+  end
+
+  def fetch(key, &)
+    raise NotImplementedError
+  end
+
+  def fetch_multi(*keys, &block)
+    raise NotImplementedError
+  end
+
+  def delete(key)
+    raise NotImplementedError
+  end
+
+  def clear
     raise NotImplementedError
   end
 end
